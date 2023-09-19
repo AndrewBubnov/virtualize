6d14d9b28c338fb7ea877e18be1d9cb7d3a626a6
Max Bubnov <m.bubnov05@gmail.com>
2023-09-19 14:49:16 +0200

    Refactor

--- src/components/Presentation/Expandable.tsx
+++ src/components/Presentation/Expandable.tsx
@@ -3,8 +3,10 @@ import styles from './Expandable.module.css';
 
 export const Expandable = ({ children }: { children: string }) => {
 	const [isExpanded, setIsExpanded] = useState(false);
+	const toggleExpand = () => setIsExpanded(prevState => !prevState);
+
 	return (
-		<div className={styles.expanded} onClick={() => setIsExpanded(prevState => !prevState)}>
+		<div className={styles.expanded} onClick={toggleExpand}>
 			{isExpanded ? `${children} ${children} ${children}` : children}
 		</div>
 	);
