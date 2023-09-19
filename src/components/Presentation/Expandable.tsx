851acb3bbc300df672bd50505c99a853ea1205e7
Max Bubnov <m.bubnov05@gmail.com>
2023-09-19 14:45:42 +0200

    Create tooltip

--- src/components/Presentation/Expandable.tsx
+++ src/components/Presentation/Expandable.tsx
@@ -1,19 +1,10 @@
-import { CSSProperties, useState } from 'react';
+import { useState } from 'react';
 import styles from './Expandable.module.css';
 
 export const Expandable = ({ children }: { children: string }) => {
 	const [isExpanded, setIsExpanded] = useState(false);
 	return (
-		<div
-			style={
-				{
-					'--content': 'Click to expand',
-					'--color': 'red',
-				} as CSSProperties
-			}
-			className={styles.expanded}
-			onClick={() => setIsExpanded(prevState => !prevState)}
-		>
+		<div className={styles.expanded} onClick={() => setIsExpanded(prevState => !prevState)}>
 			{isExpanded ? `${children} ${children} ${children}` : children}
 		</div>
 	);
