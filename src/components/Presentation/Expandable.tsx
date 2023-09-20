cd3f7fb5d465780e9d3a63c157f6fcf77b31055e
Max Bubnov <m.bubnov05@gmail.com>
2023-09-20 15:11:22 +0200

    Set tooltip label dynamically

--- src/components/Presentation/Expandable.tsx
+++ src/components/Presentation/Expandable.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
 import styles from './Expandable.module.css';
 
+enum TooltipLabel {
+	EXPAND = 'Click to expand',
+	COLLAPSE = 'Click to collapse',
+}
+
 export const Expandable = ({ children }: { children: string }) => {
 	const [isExpanded, setIsExpanded] = useState(false);
 	const toggleExpand = () => setIsExpanded(prevState => !prevState);
 
 	return (
-		<div className={styles.expanded} onClick={toggleExpand}>
+		<div
+			className={styles.expanded}
+			onClick={toggleExpand}
+			data-tooltip={isExpanded ? TooltipLabel.COLLAPSE : TooltipLabel.EXPAND}
+		>
 			{isExpanded ? `${children} ${children} ${children}` : children}
 		</div>
 	);
